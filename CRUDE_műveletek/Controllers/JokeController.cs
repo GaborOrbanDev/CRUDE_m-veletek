03d28a203ce7240d743bfdfac26c39e3e3d5e582
q4gpvc <q4gpvc@SZA0030900>
2022-11-16 15:11:19 +0100

    Add project files.

---
+++ CRUDE_műveletek/Controllers/JokeController.cs
@@ -0,0 +1,63 @@
+﻿using CRUDE_műveletek.JokeModels;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CRUDE_műveletek.Controllers
+{
+    [Route("api/jokes")]
+    [ApiController]
+    public class JokeController : ControllerBase
+    {
+        // GET: api/<JokeController>
+        [HttpGet]
+        public IActionResult Get()
+        {
+            FunnyDatabaseContext context = new FunnyDatabaseContext();
+            return Ok(context.Jokes.ToList());
+        }
+
+        // GET api/jokes/5
+        [HttpGet("{id}")]
+        public IActionResult Get(int id)
+        {
+            FunnyDatabaseContext context = new();
+            var keresettVic = (from v in context.Jokes where v.JokeSk == id select v).FirstOrDefault();
+            if (keresettVic == null)
+            {
+                return NotFound($"Nincs {id} azonosítóval vicc.");
+            }
+            else
+            {
+                return Ok(keresettVic);
+            }
+        }
+
+        // POST api/<JokeController>
+        [HttpPost]
+        public void Post([FromBody] Joke újVicc)
+        {
+            FunnyDatabaseContext context = new();
+            context.Jokes.Add(újVicc);
+            context.SaveChanges();
+        }
+
+        // PUT api/<JokeController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/jokes/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+            FunnyDatabaseContext context = new FunnyDatabaseContext();
+            var törlendőVicc = (from x in context.Jokes
+                                where x.JokeSk == id
+                                select x).FirstOrDefault();
+            context.Remove(törlendőVicc);
+            context.SaveChanges();
+        }
+    }
+}
