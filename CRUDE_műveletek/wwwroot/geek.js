decf8ba48caddb387449767fc2c78252bc44aacf
Orbán Gábor <o.gabor007@gmail.com>
2022-11-23 13:56:06 +0100

    display vicc

--- CRUDE_műveletek/wwwroot/geek.js
+++ CRUDE_műveletek/wwwroot/geek.js
@@ -4,6 +4,12 @@ const letöltésBefejeződött = (data) =>{
     console.log("Letöltés befejeződőtt")
     console.table(data)
     viccek = data
+
+    for(let vicc of viccek){
+        let viccNode = document.createElement("div")
+        viccNode.innerHTML = `<p><b>Question:</b> ${vicc.question}</p><p><b>Answer:</b> ${vicc.answer}</p><hr>`
+        document.body.appendChild(viccNode)
+    }
 }
 
 const letöltés = () => {
