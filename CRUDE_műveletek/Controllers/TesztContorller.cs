0a63d9b029280711449030d8e2adbeaab711a69f
Orbán Gábor <o.gabor007@gmail.com>
2022-11-30 14:03:21 +0100

    api/szerveridő

---
+++ CRUDE_műveletek/Controllers/TesztContorller.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CRUDE_műveletek.Controllers
+{
+    [ApiController]
+    public class TesztContorller : ControllerBase
+    {
+        [HttpGet]
+        [Route("api/szerverido")]
+        public IActionResult Szerveridő()
+        {
+            string pontosIdő = DateTime.Now.ToShortTimeString();
+            return new ContentResult {
+                ContentType = System.Net.Mime.MediaTypeNames.Text.Plain,
+                Content = pontosIdő
+            };
+            //ezt lehet egyszerűbben return Ok(pontosIdő);
+        }
+    }
+}
